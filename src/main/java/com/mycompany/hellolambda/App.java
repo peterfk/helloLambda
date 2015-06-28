3accd170f8e377a563f3035bf0ef9fddfb3a33c0
Peter Kipping <pkipping@yahoo.com>
2015-06-27 20:58:32 -0700

    Initial commit

---
+++ src/main/java/com/mycompany/hellolambda/App.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.hellolambda;
+
+import static java.lang.Character.isDigit;
+import java.util.ArrayList;
+import java.util.Arrays;
+import static java.util.Arrays.asList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import java.util.function.BinaryOperator;
+import java.util.function.Predicate;
+import static java.util.stream.Collectors.toList;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pkipping
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        Runnable noArguments = () -> System.out.println("Hello World");
+
+        BinaryOperator<Long> add = (x, y) -> x + y;
+
+        BinaryOperator<Long> addExplicit = (Long x, Long y) -> x + y;
+
+        Predicate<Integer> atLeast5 = x -> x > 5;
+
+        List<String> collected = Stream.of("a", "b", "hello")
+                .map(string -> string.toUpperCase())
+                .collect(toList());
+
+        assertEquals(asList("A", "B", "HELLO"), collected);
+
+        List<String> beginningWithNumbers
+                = Stream.of("a", "1abc", "abc1")
+                .filter(value -> isDigit(value.charAt(0)))
+                .collect(toList());
+
+        assertEquals(asList("1abc"), beginningWithNumbers);
+
+        List<String> stringCollection = new ArrayList<>();
+        stringCollection.add("ddd2");
+        stringCollection.add("aaa2");
+        stringCollection.add("bbb1");
+        stringCollection.add("aaa1");
+        stringCollection.add("bbb3");
+        stringCollection.add("ccc");
+        stringCollection.add("bbb2");
+        stringCollection.add("ddd1");
+
+        stringCollection
+                .stream()
+                .filter((s) -> s.startsWith("a"))
+                .forEach(System.out::println);
+
+        stringCollection
+                .stream()
+                .sorted()
+                .filter((s) -> s.startsWith("a"))
+                .forEach(System.out::println);
+
+        System.out.println(stringCollection);
+
+        stringCollection
+                .stream()
+                .map(String::toUpperCase)
+                .sorted((a, b) -> b.compareTo(a))
+                .forEach(System.out::println);
+
+        Optional<String> reduced
+                = stringCollection
+                .stream()
+                .sorted()
+                .reduce((s1, s2) -> s1 + "#" + s2);
+
+        reduced.ifPresent(System.out::println);
+
+        int max = 1000;
+        List<String> values = new ArrayList<>(max);
+        for (int i = 0; i < max; i++) {
+            UUID uuid = UUID.randomUUID();
+            values.add(uuid.toString());
+        }
+
+        long t0 = System.nanoTime();
+
+//        long count = values.stream().sorted().count();
+        long count = values.parallelStream().sorted().count();
+
+        System.out.println(count);
+
+        long t1 = System.nanoTime();
+
+        long millis = TimeUnit.NANOSECONDS.toMillis(t1 - t0);
+        System.out.println(String.format("sequential or parallel sort took: %d ms", millis));
+
+        List<String> myList
+                = Arrays.asList("a1", "a2", "b1", "c2", "c1");
+
+        myList
+                .stream()
+                .filter(s -> s.startsWith("c"))
+                .map(String::toUpperCase)
+                .sorted()
+                .forEach(System.out::println);
+
+    }
+
+}
