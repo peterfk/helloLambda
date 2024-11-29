c828641c0ac92719bb91b86fda10a3809f7ca366
Peter Kipping <pkipping@yahoo.com>
2024-11-29 16:41:05 -0500

    refactor to create methods

--- src/main/java/com/mycompany/hellolambda/App.java
+++ src/main/java/com/mycompany/hellolambda/App.java
@@ -30,68 +30,15 @@ public class App {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		// Java 8 stuff!!!
+		basicStreaming();
 
-		//another comment
-        //using IntelliJ now
-		Runnable noArguments = () -> System.out.println("Hello World");
+		streamCollection();
 
-		BinaryOperator<Long> add = (x, y) -> x + y;
+		streamParallel();
 
-		BinaryOperator<Long> addExplicit = (Long x, Long y) -> x + y;
+	}
 
-		Predicate<Integer> atLeast5 = x -> x > 5;
-
-		List<String> collected = Stream.of("a", "b", "hello")
-			.map(string -> string.toUpperCase())
-			.collect(toList());
-
-		assertEquals(asList("A", "B", "HELLO"), collected);
-
-		List<String> beginningWithNumbers
-			= Stream.of("a", "1abc", "abc1")
-				.filter(value -> isDigit(value.charAt(0)))
-				.collect(toList());
-
-		assertEquals(asList("1abc"), beginningWithNumbers);
-
-		List<String> stringCollection = new ArrayList<>();
-		stringCollection.add("ddd2");
-		stringCollection.add("aaa2");
-		stringCollection.add("bbb1");
-		stringCollection.add("aaa1");
-		stringCollection.add("bbb3");
-		stringCollection.add("ccc");
-		stringCollection.add("bbb2");
-		stringCollection.add("ddd1");
-
-		stringCollection
-			.stream()
-			.filter((s) -> s.startsWith("a"))
-			.forEach(System.out::println);
-
-		stringCollection
-			.stream()
-			.sorted()
-			.filter((s) -> s.startsWith("a"))
-			.forEach(System.out::println);
-
-		System.out.println(stringCollection);
-
-		stringCollection
-			.stream()
-			.map(String::toUpperCase)
-			.sorted((a, b) -> b.compareTo(a))
-			.forEach(System.out::println);
-
-		Optional<String> reduced
-			= stringCollection
-				.stream()
-				.sorted()
-				.reduce((s1, s2) -> s1 + "#" + s2);
-
-		reduced.ifPresent(System.out::println);
-
+	protected static void streamParallel() {
 		int max = 1000;
 		List<String> values = new ArrayList<>(max);
 		for (int i = 0; i < max; i++) {
@@ -102,25 +49,90 @@ public class App {
 		long t0 = System.nanoTime();
 
 //        long count = values.stream().sorted().count();
-		long count = values.parallelStream().sorted().count();
+long count = values.parallelStream().sorted().count();
 
-		System.out.println(count);
+System.out.println(count);
 
-		long t1 = System.nanoTime();
+long t1 = System.nanoTime();
 
-		long millis = TimeUnit.NANOSECONDS.toMillis(t1 - t0);
-		System.out.println(String.format("sequential or parallel sort took: %d ms", millis));
+long millis = TimeUnit.NANOSECONDS.toMillis(t1 - t0);
+System.out.println(String.format("sequential or parallel sort took: %d ms", millis));
 
-		List<String> myList
-			= Arrays.asList("a1", "a2", "b1", "c2", "c1");
+List<String> myList
+		= Arrays.asList("a1", "a2", "b1", "c2", "c1");
 
-		myList
-			.stream()
-			.filter(s -> s.startsWith("c"))
-			.map(String::toUpperCase)
-			.sorted()
-			.forEach(System.out::println);
+myList
+		.stream()
+		.filter(s -> s.startsWith("c"))
+		.map(String::toUpperCase)
+		.sorted()
+		.forEach(System.out::println);
+	}
 
+	protected static void streamCollection() {
+		List<String> stringCollection = new ArrayList<>();
+		stringCollection.add("ddd2");
+		stringCollection.add("aaa2");
+		stringCollection.add("bbb1");
+		stringCollection.add("aaa1");
+		stringCollection.add("bbb3");
+		stringCollection.add("ccc");
+		stringCollection.add("bbb2");
+		stringCollection.add("ddd1");
+		
+		stringCollection
+				.stream()
+				.filter((s) -> s.startsWith("a"))
+				.forEach(System.out::println);
+		
+		stringCollection
+				.stream()
+				.sorted()
+				.filter((s) -> s.startsWith("a"))
+				.forEach(System.out::println);
+		
+		System.out.println(stringCollection);
+		
+		stringCollection
+				.stream()
+				.map(String::toUpperCase)
+				.sorted((a, b) -> b.compareTo(a))
+				.forEach(System.out::println);
+		
+		Optional<String> reduced
+				= stringCollection
+						.stream()
+						.sorted()
+						.reduce((s1, s2) -> s1 + "#" + s2);
+		
+		reduced.ifPresent(System.out::println);
+	}
+
+	protected static void basicStreaming() {
+		// Java 8 stuff!!!
+		
+		//another comment
+		//using IntelliJ now
+		Runnable noArguments = () -> System.out.println("Hello World");
+		
+		BinaryOperator<Long> add = (x, y) -> x + y;
+		
+		BinaryOperator<Long> addExplicit = (Long x, Long y) -> x + y;
+		
+		Predicate<Integer> atLeast5 = x -> x > 5;
+		
+		List<String> collected = Stream.of("a", "b", "hello")
+				.map(string -> string.toUpperCase())
+				.collect(toList());
+		
+		assertEquals(asList("A", "B", "HELLO"), collected);
+		
+		List<String> beginningWithNumbers
+				= Stream.of("a", "1abc", "abc1")
+						.filter(value -> isDigit(value.charAt(0)))
+						.collect(toList());
+		
+		assertEquals(asList("1abc"), beginningWithNumbers);
 	}
 
 }
